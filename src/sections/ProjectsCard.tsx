d30556106fbb28b6d31561f115e25549ebcd7e86
N3Shemmy3 <n3shemmy3@gmail.com>
2022-12-07 19:48:58 +0200

    finaly made the pinned repo fetching to work

--- src/sections/ProjectsCard.tsx
+++ src/sections/ProjectsCard.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import ProjectCard from "../components/ProjectCard";
 interface Project {
 	owner: string;
@@ -16,59 +16,21 @@ interface State {
 	projects: Project[];
 }
 export default function ProjectsCard(props: any, state: State) {
-	let list: any[] = [
-		{
-			owner: "N3Shemmy3",
-			repo: "n3shemmy3.github.io",
-			description:
-				"My personal website and portfolio where i showcase my skills and intersts",
-			link: "https://github.com/N3Shemmy3/n3shemmy3.github.io",
-			image:
-				"https://opengraph.githubassets.com/1/N3Shemmy3/n3shemmy3.github.io",
-			language: "TypeScript",
-			languageColor: "#3178c6",
-			stars: "1",
-			forks: 0,
-		},
-		{
-			owner: "N3Shemmy3",
-			repo: "MNotes",
-			description:
-				"A material design multiplatform note taking app built using the vue js framework",
-			link: "https://github.com/N3Shemmy3/MNotes",
-			image: "https://opengraph.githubassets.com/1/N3Shemmy3/MNotes",
-			language: "Vue",
-			languageColor: "#41b883",
-			stars: "2",
-			forks: 0,
-		},
-		{
-			owner: "N3Shemmy3",
-			repo: "StackHub-web",
-			description:
-				"StackHub is a soon to be cross-platform net for software engneers to collaborate and socialize",
-			link: "https://github.com/N3Shemmy3/StackHub-web",
-			image: "https://opengraph.githubassets.com/1/N3Shemmy3/StackHub-web",
-			language: "Vue",
-			languageColor: "#41b883",
-			stars: "1",
-			forks: 0,
-		},
-	];
-	/**
-		 * 
-		 axios
-		 .get("https://gh-pinned-repos.egoist.dev/?username=N3Shemmy3")
-		 .then((res) => {
-			 console.log(res);
-			 list = res.data;
-			 this.setProjects(list);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
+	const [list, setList] = useState([]);
+
+	useEffect(() =>{
+		axios
+		.get("https://gh-pinned-repos.egoist.dev/?username=N3Shemmy3")
+		.then((res) => {
+			console.log(res);
+			setList(res.data);
+		   })
+		   .catch((error) => {
+			   console.log(error);
+		   });
+	}, [])
 			
-			*/
+		
 	return (
 		<div>
 			<div className="w-full max-w-md mx-auto h-fit p-4 flex flex-col justify-center content-center">
@@ -79,7 +41,6 @@ export default function ProjectsCard(props: any, state: State) {
 					A list of projects i have worked on
 				</span>
 			</div>
-
 			<div className="w-full max-w-6xl m-auto gap-4 h-fit p-4 md:p-8 grid md:grid-cols-2 lg:grid-cols-3">
 				{list.map((project: any, idx: number) => (
 					<ProjectCard project={project} key={idx} />
