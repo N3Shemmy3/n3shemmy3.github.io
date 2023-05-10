c9b21e2d9fe9c7209d3d4be9f5554ee0903ef4d8
N3Shemmy3 <n3shemmy3@gmail.com>
2023-05-10 22:58:59 +0200

    initialized Repo

---
+++ components/Toolbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="Toolbar">
+        <MenuIcon />
+        <span class="text-xl">Toolbar</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
+
+</script>
+<style scoped>
+@import '@/assets/css/tailwind.css';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="">
+        <Toolbar />
+        <div>
+
+            <h1>Hellow World
+                tehsfgkslhggwlhgghsifglsffhaiblfaadhoihgidfgSFIHGIHSGJSFHGHSFGSFHGIDHahihfgihdifgh;dinhfkdijhfdindkhdg</h1>
+        </div>
+
+    </div>
+</template>
