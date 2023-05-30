23829180970f76bbcc4bd8d0906dd5f1e9d4f5f2
N3Shemmy3 <n3shemmy3@gmail.com>
2023-05-30 23:40:37 +0200

    tried changing the tab icon (failed i think)

--- app.vue
+++ app.vue
@@ -4,6 +4,10 @@
 			class="h-screen w-full select-none text-colorOnBackgroundLight dark:text-colorOnBackgroundDark bg-colorBackgroundLight dark:bg-colorBackgroundDark"
 		>
 			<Head>
+				<link
+					rel="icon"
+					href="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgMjU2IDI1NiI+PHBhdGggZmlsbD0iIzg4ODg4OCIgZD0iTTEyOCAxMjhhOCA4IDAgMCAxLTMgNi4yNWwtNDAgMzJhOCA4IDAgMSAxLTEwLTEyLjVMMTA3LjE5IDEyOEw3NSAxMDIuMjVhOCA4IDAgMSAxIDEwLTEyLjVsNDAgMzJhOCA4IDAgMCAxIDMgNi4yNVptNDggMjRoLTQwYTggOCAwIDAgMCAwIDE2aDQwYTggOCAwIDAgMCAwLTE2Wm01Ni05NnYxNDRhMTYgMTYgMCAwIDEtMTYgMTZINDBhMTYgMTYgMCAwIDEtMTYtMTZWNTZhMTYgMTYgMCAwIDEgMTYtMTZoMTc2YTE2IDE2IDAgMCAxIDE2IDE2Wm0tMTYgMTQ0VjU2SDQwdjE0NGgxNzZaIi8+PC9zdmc+"
+				/>
 				<Title>Shemmy Nyirenda</Title>
 				<Meta
 					name="description"
