899dd026da4b69d8d3f5ea64162198ea8357054e
N3Shemmy3 <n3shemmy3@gmail.com>
2023-05-30 21:08:08 +0200

    added flex grow elements around the languages div

--- pages/index.vue
+++ pages/index.vue
@@ -10,17 +10,23 @@
         id="about"
         class="flex flex-col space-y-4 justify-center [&>*]:mx-auto"
       >
+      <div class="sm:flex">
+        
         <img
-          class="h-60 w-60 rounded-full"
-          src="https://avatars.githubusercontent.com/u/67279072?v=4"
-          alt=""
+        class="h-60 w-60 rounded-full"
+        src="https://avatars.githubusercontent.com/u/67279072?v=4"
+        alt=""
         />
+        <div>
         <h1 class="text-3xl text-center sm:text-[40px]">Shemmy Nyirenda</h1>
         <p class="text-base text-center">
           A Front-End Software Developer based in Zambia
         </p>
-
+      </div>
+        
+      
         <div class="flex m-auto pt-4 space-x-10">
+          <div class="flex-grow"/>
           <Icon
             title="Java"
             name="nonicons:java-16"
@@ -42,7 +48,9 @@
             size="30"
             class="transition-color noSelect duration-250 cursor-pointer hover:text-colorPrimaryLight dark:hover:text-colorPrimaryDark"
           />
+           <div class="flex-grow"/>
         </div>
+      </div>
       </div>
       <div id="projects-title" class="mt-24">
         <h1 class="w-full text-start text-3xl m-0 sm:text-3xl">#Projects</h1>
