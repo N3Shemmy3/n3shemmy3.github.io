108abb529d466271d3eb0e03ebd6075b42e2aa7e
N3Shemmy3 <n3shemmy3@gmail.com>
2023-05-30 22:23:13 +0200

    cleaner way for lising items

--- app.vue
+++ app.vue
@@ -1,22 +1,28 @@
 <template>
-  <div
-    class="h-screen w-full select-none text-colorOnBackgroundLight dark:text-colorOnBackgroundDark bg-colorBackgroundLight dark:bg-colorBackgroundDark"></div>
+	<div
+		class="h-screen w-full select-none text-colorOnBackgroundLight dark:text-colorOnBackgroundDark bg-colorBackgroundLight dark:bg-colorBackgroundDark"
+	>
+		<Head>
+			<Title>Shemmy Nyirenda</Title>
+			<Meta
+				name="description"
+				content="A website i built with nuxt.js to showcase My skills and passions"
+			/>
+			<meta name="theme-color" content="bg-colorBackgroundLight" />
+			<Style
+				type="text/css"
+				children="body { @apply bg-colorBackgroundLight dark:bg-colorBackgroundDark;  }"
+			/>
+		</Head>
 
-    <Head>
-      <Title>Shemmy Nyirenda</Title>
-      <Meta name="description" content="A website i built with nuxt.js to showcase My skills and passions" />
-      <meta name="theme-color" content="bg-colorBackgroundLight" />
-      <Style type="text/css" children="body { @apply bg-colorBackgroundLight dark:bg-colorBackgroundDark;  }" />
-    </Head>
+		<div class="max-w-screen-md">
+			<Navbar />
+			<div class="justify-center pt-14 px-4 pb-4 sm:pt-28 sm:px-6">
+				<NuxtPage />
+			</div>
+			<FooterBar />
+		</div>
+	</div>
+</template>
 
-    <div class="max-w-screen-md">
-      <Navbar />
-      <div class="justify-center pt-14 px-4 pb-4 sm:pt-20 sm:px-6">
-        <NuxtPage />
-      </div>
-      <FooterBar />
-    </div>
-  </template>
-
-<script setup>
-</script>
+<script setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,44 @@
 <template>
 	<div
-		class="w-full h-full select-none text-colorOnBackgroundLight dark:text-colorOnBackgroundDark bg-colorBackgroundLight dark:bg-colorBackgroundDark justify-center pt-14 px-4 pb-4 sm:pt-28 sm:px-6"
+		id="content"
+		class="select-none overflow-auto flex flex-col m-auto max-w-3xl justify-center"
 	>
-		<div
-			id="content"
-			class="overflow-auto flex flex-col m-auto max-w-3xl justify-center"
-		>
-			<div id="about" class="flex flex-col space-y-4 justify-center">
-				<div class="sm:flex sm:space-x-10 space-y-2">
-					<img
-						id="image"
-						class="h-60 w-60 m-auto sm:m-0 rounded-full"
-						src="https://avatars.githubusercontent.com/u/67279072?v=4"
-						alt=""
-					/>
-					<div class="sm:space-y-2 flex flex-col justify-center">
-						<div id="info" class="space-y-1">
-							<h1 class="text-3xl text-center sm:text-[45px]"
-								>Shemmy Nyirenda</h1
-							>
-							<p class="text-base text-center">
-								A Front-End Software Developer based in Zambia
-							</p>
-						</div>
-						<div id="languages" class="flex m-auto pt-4 space-x-10">
-							<div class="flex-grow" />
-							<Icon
-								v-for="language in languages"
-								:title="language.title"
-								:name="language.src"
-								size="30"
-								class="transition-color noSelect duration-250 cursor-pointer hover:text-colorPrimaryLight dark:hover:text-colorPrimaryDark"
-							/>
-							<div class="flex-grow" />
-						</div>
+		<div id="about" class="flex flex-col space-y-4 justify-center">
+			<div class="sm:flex sm:space-x-10 space-y-2">
+				<img
+					id="image"
+					class="h-60 w-60 m-auto sm:m-0 rounded-full"
+					src="https://avatars.githubusercontent.com/u/67279072?v=4"
+					alt=""
+				/>
+				<div class="sm:space-y-2 flex flex-col justify-center">
+					<div id="info" class="space-y-1">
+						<h1 class="text-3xl text-center sm:text-[45px]">Shemmy Nyirenda</h1>
+						<p class="text-base text-center">
+							A Front-End Software Developer based in Zambia
+						</p>
+					</div>
+					<div
+						id="languages"
+						class="flex mx-auto w-9/12 content-center pt-4 justify-between"
+					>
+						<Icon
+							v-for="language in languages"
+							:key="language.src"
+							:title="language.title"
+							:name="language.src"
+							size="30"
+							class="transition-color noSelect duration-250 cursor-pointer hover:text-colorPrimaryLight dark:hover:text-colorPrimaryDark"
+						/>
 					</div>
 				</div>
 			</div>
-			<div id="projects-title" class="mt-24">
-				<h1 class="w-full text-start text-3xl m-0 sm:text-3xl">#Projects</h1>
-				<p class="text-base mt-1">Projects iv'e worked on.</p>
-			</div>
-			<div class="h-[100px]"></div>
 		</div>
+		<div id="projects-title" class="mt-24">
+			<h1 class="w-full text-start text-3xl m-0 sm:text-3xl">#Projects</h1>
+			<p class="text-base mt-1">Projects iv'e worked on.</p>
+		</div>
+		<div class="h-[100px]"></div>
 	</div>
 </template>
 
