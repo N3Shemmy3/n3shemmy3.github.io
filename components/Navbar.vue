8a15242e597674a5367b97bc7b20dbb052de6bf0
N3Shemmy3 <n3shemmy3@gmail.com>
2023-05-30 22:27:26 +0200

    toolbar will center content on extra lg screen

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,32 +1,34 @@
 <template>
-  <div
-    id="Toolbar"
-    class="fixed top-0 z-50 h-14 w-full flex px-4 py-2 space-x-4 items-center bg-colorSurfaceLight dark:bg-colorSurfaceDark text-colorOnSurfaceLight dark:text-colorOnSurfaceDark shadow-sm"
-  >
-    <div
-      class="flex noSelect justify-center h-12 w-12 rounded-full cursor-pointer hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
-    >
-      <Icon class="m-auto" name="ph:terminal-window" size="30px" />
-    </div>
-    <div class="flex-grow" />
-    <div class="flex space-x-1">
-      <div
-        class="flex noSelect jsutify-center h-12 w-12 rounded-full hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
-      >
-        <Icon class="m-auto" name="ph:github-logo" size="24px" />
-      </div>
-      <div
-        class="flex noSelect justify-center h-12 w-12 rounded-full [&>*]:hover:cursor-pointer hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
-      >
-        <Icon class="m-auto" name="ph:telegram-logo" size="24px" />
-      </div>
-      <div
-        class="flex noSelect justify-center h-12 w-12 rounded-full hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
-      >
-        <Icon class="m-auto" name="ph:linkedin-logo" size="24px" />
-      </div>
-    </div>
-    <!--
+	<div
+		id="Toolbar"
+		class="fixed top-0 z-50 h-14 w-full bg-colorSurfaceLight dark:bg-colorSurfaceDark text-colorOnSurfaceLight dark:text-colorOnSurfaceDark shadow-sm"
+	>
+		<div class="max-w-screen-xl m-auto flex px-4 py-2 space-x-4 items-center">
+			<div
+				class="flex noSelect justify-center h-12 w-12 rounded-full cursor-pointer hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
+			>
+				<Icon class="m-auto" name="ph:terminal-window" size="30px" />
+			</div>
+			<div class="flex-grow" />
+			<div class="flex space-x-1">
+				<div
+					class="flex noSelect jsutify-center h-12 w-12 rounded-full hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
+				>
+					<Icon class="m-auto" name="ph:github-logo" size="24px" />
+				</div>
+				<div
+					class="flex noSelect justify-center h-12 w-12 rounded-full [&>*]:hover:cursor-pointer hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
+				>
+					<Icon class="m-auto" name="ph:telegram-logo" size="24px" />
+				</div>
+				<div
+					class="flex noSelect justify-center h-12 w-12 rounded-full hover:bg-colorPrimaryContainerLight dark:hover:bg-colorPrimaryContainerDark"
+				>
+					<Icon class="m-auto" name="ph:linkedin-logo" size="24px" />
+				</div>
+			</div>
+		</div>
+		<!--
 
             <div v-for="(i, index) in menuItems">
                 <h6 class="
@@ -45,26 +47,26 @@
                 ">{{ i }}</h6>
             </div>
         -->
-  </div>
+	</div>
 </template>
 
 <script setup lang="ts">
-const menuItems = ["About", "Projects", "BlogPost"];
-function empty() {}
+	const menuItems = ["About", "Projects", "BlogPost"];
+	function empty() {}
 </script>
 
 <style scoped>
-.noSelect {
-  -webkit-tap-highdark-color: transparent;
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
+	.noSelect {
+		-webkit-tap-highdark-color: transparent;
+		-webkit-touch-callout: none;
+		-webkit-user-select: none;
+		-khtml-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
 
-.noSelect:focus {
-  outline: none !important;
-}
+	.noSelect:focus {
+		outline: none !important;
+	}
 </style>
