82c1a302a560bfb92f118f026eb4f890bd6c469d
N3Shemmy3 <n3shemmy3@gmail.com>
2023-06-01 10:04:32 +0200

    pain

--- components/Projects.vue
+++ components/Projects.vue
@@ -2,22 +2,41 @@
 	<div id="projects-title" class="mt-24">
 		<h1 class="w-full text-start text-3xl m-0 sm:text-3xl">#Projects</h1>
 		<p class="text-base mt-1">Projects iv'e worked on.</p>
-		<div class="flex flex-col shadow-sm m-2">
+		<div class="grid gap-4 grid-responsive py-4">
 			<div
+				v-for="n in 5"
 				id="card"
-				class="h-2/6 p-2 rounded-t-lg bg-colorPrimaryContainerDark"
+				class="w-full sm:w-60 rounded-md shadow-sm border border-colorOutlineLight dark:border-colorOutlineDark"
 			>
-				<Icon name="nonicons:java-16" size="30px" />
-				<h6 class="text-lg">Project Name</h6>
-			</div>
-			<div class="h-4/6 p-2 rounded-b-lg bg-colorSecondaryContainerDark">
-				<p
-					>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores,
-					itaque adipisci? Odio, reprehenderit, animi aut illum ut corrupti
-					earum nemo odit obcaecati quia iste dignissimos sit dolor atque
-					pariatur ducimus!</p
-				>
+				<div id="img-content" class="h-52 w-full">
+					<img src="/svg/undraw_floating_re_xtcj.svg" />
+				</div>
+				<div id="text-content" class="p-4 space-y-2">
+					<NuxtLink
+						class="cursor-pointer w-fit hover:text-colorPrimaryLight dark:hover:text-colorPrimaryDark"
+					>
+						<h6 class="text-2xl font-semibold">Project Name</h6>
+					</NuxtLink>
+					<p class="text-base"
+						>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p
+					>
+					<div class="flex pt-2 space-x-2">
+						<div class="flex space-x-1">
+							<Icon name="ph:star" />
+							<h6 class="text-sm">200</h6>
+						</div>
+						<div class="flex-grow" />
+						<Icon name="nonicons:javascript-16" />
+						<Icon name="nonicons:html-16" />
+						<Icon name="nonicons:css-16" />
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
+<style>
+	.grid-responsive {
+		grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+	}
+</style>
