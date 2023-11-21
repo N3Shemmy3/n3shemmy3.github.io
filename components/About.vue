3d7ac12db4bddfefc289b21d161bb72a7a934885
Shemmy <n3shemmy3@gmail.com>
2023-11-21 07:44:18 +0200

    contact button now sends correct case

--- components/About.vue
+++ components/About.vue
@@ -40,7 +40,7 @@
 				<button
 					v-Ripple
 					class="text-base sm:text-lg outline-none px-8 py-2 rounded-lg bg-colorPrimaryLight text-colorOnPrimaryLight"
-					@click="openLink('email', true)"
+					@click="openLink('contact', true)"
 				>Contact
 				</button>
 			</div>
