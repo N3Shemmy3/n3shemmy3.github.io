ea7a3d981c5542187b2c693b231dd4d33c6c34a4
Shemmy <n3shemmy3@gmail.com>
2024-02-18 10:22:51 +0200

    added some margin between the heading and the list of posts

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <AppContainer class="flex flex-col">
+    <AppContainer class="flex flex-col  space-y-4 md:space-y-8">
         <div
             id="heading-wrapper"
             class="pt-10 md:pt-20 space-y-1"
@@ -7,6 +7,7 @@
             <h1 class="text-4xl font-bold">Blog Posts</h1>
             <p class="text-base">A small area where i share my thoughts.</p>
         </div>
+
         <main>
             <ul class="mx-auto grid gap-4 grid-responsive py-4">
                 <Post
