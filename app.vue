1d9c15975ef1d566b0e604d657fdb48c5b15df43
Shemmy <Shemmynyirenda3@gmail.com>
2024-07-03 12:39:06 +0200

    fixed content position on large screen make navabr and content the same width contact button directs to telegram instead of email

--- app.vue
+++ app.vue
@@ -1,48 +1,43 @@
 <template>
-	<div
-		class="w-full select-none text-colorOnBackgroundLight dark:text-colorOnBackgroundDark bg-colorBackgroundLight dark:bg-colorBackgroundDark">
-		<div class="max-w-screen-2xl">
-			<Navbar />
-			<div class="justify-center pt-14 pb-4 px-4 sm:px-6">
-				<NuxtPage />
-			</div>
-			<FooterBar />
-		</div>
-	</div>
+  <div
+    class="w-full select-none text-colorOnBackgroundLight dark:text-colorOnBackgroundDark bg-colorBackgroundLight dark:bg-colorBackgroundDark"
+  >
+    <Navbar />
+    <div class="justify-center pt-14 pb-4 px-4 sm:px-6">
+      <NuxtPage />
+    </div>
+    <FooterBar />
+  </div>
 </template>
 
 <script setup>
 useListen("modal:event", (modal) => {
-	switch (modal.name) {
-		case "contact":
-			var email = "n3shemmy3@gmail.com"
-			var subject = "Job Offer"
-			var body = "Hi Shem,"
-			window.location.href =
-				"mailto:" + email + "?" + "subject=" + subject + "&body=" + body;
-			break;
+  switch (modal.name) {
+    case "contact":
+      var email = "n3shemmy3@gmail.com";
+      var subject = "Job Offer";
+      var body = "Hi Shem,";
+      window.location.href =
+        "mailto:" + email + "?" + "subject=" + subject + "&body=" + body;
+      break;
 
-		case "resume":
-
-			break;
-	}
+    case "resume":
+      break;
+  }
 });
 
 useSeoMeta({
-	title: "Shemmy Nyirenda",
-	ogTitle: "Shemmy Nyirenda",
-	description:
-		"A website i built with nuxt.js to showcase My skills and passions",
-	ogDescription:
-		"A website i built with nuxt.js to showcase My skills and passions",
-	ogImage:
-		"https://api.iconify.design/ph:terminal-window.svg?color=%23888888",
-	twitterCard: "summary_large_image",
+  title: "Shemmy Nyirenda",
+  ogTitle: "Shemmy Nyirenda",
+  description: "A website i built with nuxt.js to showcase My skills and passions",
+  ogDescription: "A website i built with nuxt.js to showcase My skills and passions",
+  ogImage: "https://api.iconify.design/ph:terminal-window.svg?color=%23888888",
+  twitterCard: "summary_large_image",
 });
 </script>
 <style scoped>
 dialog::backdrop {
-	backdrop-filter: blur(8px);
-	background: rgba(23, 22, 22, 0.469);
+  backdrop-filter: blur(8px);
+  background: rgba(23, 22, 22, 0.469);
 }
 </style>
--- components/About.vue
+++ components/About.vue
@@ -1,81 +1,73 @@
 <template>
-	<div
-		id="about"
-		class="flex flex-col mb-2"
-	>
-		<div class="space-y-4 flex flex-col justify-center m-auto">
-			<div
-				ref="info"
-				class="mt-20 select-text"
-			>
-				<h1 class="text-[55px] sm:text-[70px] text-center text-colorPrimaryLight dark:text-colorPrimaryDark">
-					Hello there,</h1>
-				<p class="text-lg whitespace-normal sm:text-2xl text-center">
-					I'm Shemmy Nyirenda a passionate web developer
-				</p>
-			</div>
-			<div
-				ref="languages-div"
-				class="flex mx-auto mb-4 content-center p-4 space-x-8"
-			>
-				<div
-					v-for="language in languages"
-					v-bind:key="language.icon"
-				>
-					<Icon
-						:title="language.title"
-						:name="language.icon"
-						size="30px"
-						class="transition-color noSelect duration-250 hover:text-colorPrimaryLight dark:hover:text-colorPrimaryDark"
-					/>
-				</div>
-			</div>
-			<div class="flex mx-auto ">
-				<button
-					v-if="false"
-					class="text-base sm:text-lg outline-none px-8 py-2 rounded-lg border hover:text-colorPrimaryLight hover:dark:text-colorPrimaryDark hover:border-colorPrimaryLight hover:dark:border-colorPrimaryDark"
-					@click="openLink('resume', true)"
-				>Resume
-				</button>
-				<div class="w-4" />
-				<button
-					v-Ripple
-					class="flex-1 md:w-30 text-base sm:text-lg outline-none px-8 py-2 rounded-lg bg-colorPrimaryLight text-colorOnPrimaryLight"
-					@click="openLink('contact', true)"
-				>Contact me
-				</button>
-			</div>
-		</div>
-	</div>
+  <div id="about" class="flex flex-col mb-2">
+    <div class="space-y-4 flex flex-col justify-center m-auto">
+      <div ref="info" class="mt-20 select-text">
+        <h1
+          class="text-[55px] sm:text-[70px] text-center text-colorPrimaryLight dark:text-colorPrimaryDark"
+        >
+          Hello there,
+        </h1>
+        <p class="text-lg whitespace-normal sm:text-2xl text-center">
+          I'm Shemmy Nyirenda a passionate web developer
+        </p>
+      </div>
+      <div ref="languages-div" class="flex mx-auto mb-4 content-center p-4 space-x-8">
+        <div v-for="language in languages" v-bind:key="language.icon">
+          <Icon
+            :title="language.title"
+            :name="language.icon"
+            size="30px"
+            class="transition-color noSelect duration-250 hover:text-colorPrimaryLight dark:hover:text-colorPrimaryDark"
+          />
+        </div>
+      </div>
+      <div class="flex mx-auto">
+        <button
+          v-if="false"
+          class="text-base sm:text-lg outline-none px-8 py-2 rounded-lg border hover:text-colorPrimaryLight hover:dark:text-colorPrimaryDark hover:border-colorPrimaryLight hover:dark:border-colorPrimaryDark"
+          @click="openLink('resume', true)"
+        >
+          Resume
+        </button>
+        <div class="w-4" />
+        <button
+          v-Ripple
+          class="flex-1 md:w-30 text-base sm:text-lg outline-none px-8 py-2 rounded-lg bg-colorPrimaryLight text-colorOnPrimaryLight"
+        >
+          <NuxtLink href="https://t.me/N3Shemmy3"> Contact me </NuxtLink>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 interface Menu {
-	icon: string;
-	title: string;
-	url: string;
+  icon: string;
+  title: string;
+  url: string;
 }
 const languages = ref([
-	{
-		title: "Java",
-		icon: "nonicons:java-16",
-	},
-	{
-		title: "JavaScript",
-		icon: "nonicons:javascript-16",
-	},
-	{
-		title: "Html",
-		icon: "nonicons:html-16",
-	},
-	{
-		title: "Css",
-		icon: "nonicons:css-16",
-	},
+  {
+    title: "Java",
+    icon: "nonicons:java-16",
+  },
+  {
+    title: "JavaScript",
+    icon: "nonicons:javascript-16",
+  },
+  {
+    title: "Html",
+    icon: "nonicons:html-16",
+  },
+  {
+    title: "Css",
+    icon: "nonicons:css-16",
+  },
 ]);
 
 function openLink(name: string, show: boolean) {
-	useEvent("modal:event", { name: name, visible: show });
+  useEvent("modal:event", { name: name, visible: show });
 }
 </script>
 
