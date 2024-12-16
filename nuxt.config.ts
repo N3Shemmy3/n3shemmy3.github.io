13f1eb68cfaac9e01b9d24147ef552664140c50a
Shemmy <shemmynyirenda3@gmail.com>
2024-12-16 16:06:14 +0200

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  ssr: false,
   devtools: { enabled: true },
   modules: ["@nuxt/icon", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
   components: [
